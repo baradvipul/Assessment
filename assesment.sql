7ec568e9d06d60d856b8bb61eededb76a6740681
baradvipul <vipulbarad3494@gmail.com>
2025-11-18 10:17:14 +0530

    Add files via upload

---
+++ assesment.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2025 at 05:42 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assesment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `PRO_ID` int(11) NOT NULL,
+  `PRO_NAME` varchar(50) DEFAULT NULL,
+  `PRO_PRICE` decimal(10,2) DEFAULT NULL,
+  `PRO_COM` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`PRO_ID`, `PRO_NAME`, `PRO_PRICE`, `PRO_COM`) VALUES
+(101, 'Mother Board', 3200.00, 15),
+(102, 'Key Board', 450.00, 16),
+(103, 'ZIP drive', 250.00, 14),
+(104, 'Speaker', 550.00, 16),
+(105, 'Monitor', 5000.00, 11),
+(106, 'DVD drive', 900.00, 12),
+(107, 'CD drive', 800.00, 12),
+(108, 'Printer', 2600.00, 13),
+(109, 'Refill cartridge', 350.00, 13),
+(110, 'Mouse', 250.00, 12);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`PRO_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
